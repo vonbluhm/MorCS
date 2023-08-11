4496742a85e5f9ae8e8b610fe497d6d6c1f484fd
vonbluhm <yvieharper@protonmail.com>
2023-08-11 23:45:42 +0300

    Beep pitch is now definitely persistent

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -15,10 +15,13 @@ func _ready():
 		config.set_value("Player1", "hide_codes", false)
 	if config.get_value("Player1", "high_score") == null:
 		config.set_value("Player1", "high_score", 0)
+	if config.get_value("Player1", "beep_frequency") == null:
+		config.set_value("Player1", "beep_frequency", 800)
 	config.save("user://settings.ini")
 	settings.dict_option = config.get_value("Player1", "character_set")
 	settings.hide_codes = config.get_value("Player1", "hide_codes")
 	settings.high_score = config.get_value("Player1", "high_score")
+	settings.beep_frequency = config.get_value("Player1", "beep_frequency")
 
 
 func _on_start_button_pressed():
