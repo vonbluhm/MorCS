9774a30011d96d5534ec116c46538d1bb85f1cac
vonbluhm <yvieharper@protonmail.com>
2023-08-11 22:56:57 +0300

    Made a custom signal to submit input masks

--- scripts/input_handler.gd
+++ scripts/input_handler.gd
@@ -3,11 +3,18 @@ extends Node2D
 var output = "" 
 const DOT = "."
 const DASH = "-"
+
 @onready var stage = get_parent()
 @onready var label = $Label
 @onready var dot_dash = $DotDashTimer
 @onready var end_of_symbol = $SymbolSpaceTimer
 @onready var generator_scene = preload("res://scenes/generator.tscn")
+var generator = null
+signal output_changed
+
+
+func _ready():
+	output_changed.connect(_on_output_changed)
 
 
 func _process(_delta):
@@ -15,22 +22,23 @@ func _process(_delta):
 		dot_dash.start()
 		end_of_symbol.stop()
 		var instance = generator_scene.instantiate()
-		instance.add_to_group("generator")
+		generator = instance
 		add_child(instance)
 	if Input.is_action_just_released("button_pressed"):
 		if not dot_dash.is_stopped():
 			dot_dash.stop()
 			output += DOT
 			label.text = output
+			output_changed.emit()
 		end_of_symbol.start()
-		for child in get_children():
-			if child.is_in_group("generator"):
-				child.queue_free()
+		if generator != null:
+			generator.queue_free()
 
 
 func _on_dot_dash_timer_timeout():
 	output += DASH
 	label.text = output
+	output_changed.emit()
 
 
 func _on_symbol_space_timer_timeout():
@@ -38,8 +46,9 @@ func _on_symbol_space_timer_timeout():
 		stage.check_matches(output)
 	output = ""
 	label.text = output
+	output_changed.emit()
 
 
-func _on_label_resized():
+func _on_output_changed():
 	if stage != null and stage.has_method("toggle_collision_for_active_symbols"):
 		stage.toggle_collision_for_active_symbols(output)
